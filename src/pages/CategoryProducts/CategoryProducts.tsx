69da7374f6dfd3be732c0ef6f55297a2f03aedc1
sabuhimv <sebuhimv@gmail.com>
2024-10-05 22:48:16 +0400

    Product Detail page

--- src/pages/CategoryProducts/CategoryProducts.tsx
+++ src/pages/CategoryProducts/CategoryProducts.tsx
@@ -18,16 +18,14 @@ const CategoryProducts = () => {
     const category = location.pathname
 
     return (
-        <div className='container'>
+        <div className='container category-products'>
             <p className='page-heading'>Home / <span>Category</span> / <span className='page-heading__active'>{category.slice(10, category.length).replace(/-/g, " ").toUpperCase()}</span></p>
-            <div className='category-products'>
+            <div className='products'>
                 {
                     products.map((product) => (
-                        <div className='category-product' key={product.id}>
-                            {category.slice(10, category.length).split("-")[0] == product?.category.toLowerCase().split(" &")[0] ?
-                                <ProductCart key={product.id} id={product.id} title={product.title} image={product.image} price={product.price} old_price={product?.old_price} discount={product.discount} stars={product.stars} reviews={product.reviews} />
-                                : null}
-                        </div>
+                        (category.slice(10, category.length).split("-")[0] == product?.category.toLowerCase().split(" &")[0] ?
+                            <ProductCart key={product.id} id={product.id} title={product.title} image={product.image} price={product.price} old_price={product?.old_price} discount={product.discount} stars={product.stars} reviews={product.reviews} />
+                            : null)
                     ))
                 }
             </div>
